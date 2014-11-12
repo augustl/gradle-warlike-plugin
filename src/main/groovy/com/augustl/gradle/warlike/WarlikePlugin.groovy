f0482331136b9102b35c10871b51bcfc86dcd0df
August Lilleaas <august@augustl.com>
2014-11-12 11:00:05 +0100

    Initial commit.

---
+++ src/main/groovy/com/augustl/gradle/warlike/WarlikePlugin.groovy
@@ -0,0 +1,44 @@
+package com.augustl.gradle.warlike
+
+import org.gradle.api.Task
+import org.gradle.api.internal.file.FileResolver
+import org.gradle.api.Action
+import org.gradle.api.Plugin
+import org.gradle.api.Project
+import org.gradle.api.internal.plugins.DslObject
+import org.gradle.api.internal.tasks.DefaultSourceSet
+import org.gradle.api.plugins.ApplicationPlugin
+import org.gradle.api.plugins.GroovyPlugin
+import org.gradle.api.plugins.JavaPluginConvention
+import org.gradle.api.plugins.WarPlugin
+import org.gradle.api.tasks.JavaExec
+import org.gradle.api.tasks.SourceSet
+import org.gradle.plugins.ide.idea.IdeaPlugin
+
+import javax.inject.Inject
+
+class WarlikePlugin implements Plugin<Project> {
+    @Override
+    void apply(Project project) {
+        project.plugins.apply(GroovyPlugin)
+
+        JavaPluginConvention convention = project.getConvention().getPlugin(JavaPluginConvention.class)
+
+        convention.getSourceSets().main {
+            output.classesDir = "build/main-web/WEB-INF/classes"
+            output.resourcesDir = "build/main-web/WEB-INF/resources"
+        }
+
+        SourceSet mainSourceSet = convention.getSourceSets().getByName(SourceSet.MAIN_SOURCE_SET_NAME)
+
+        Task run = project.tasks.create("runWarlike", JavaExec)
+        run.dependsOn("classes")
+        run.description = "Runs an auto-reloading WAR container like environment"
+        run.classpath = mainSourceSet.runtimeClasspath
+        run.classpath += project.buildscript.configurations.classpath
+        run.args = [9085]
+        run.main = "com.augustl.gradle.warlike.WarlikeServer"
+        String springloadedJar = project.buildscript.configurations.classpath.find { it.name.startsWith("springloaded")}
+        run.jvmArgs = ["-javaagent:${springloadedJar}", "-noverify"]
+    }
+}
---
+++ src/main/groovy/com/augustl/gradle/warlike/WarlikeServer.groovy
@@ -0,0 +1,43 @@
+package com.augustl.gradle.warlike
+
+import org.eclipse.jetty.annotations.AnnotationConfiguration
+import org.eclipse.jetty.plus.webapp.EnvConfiguration
+import org.eclipse.jetty.plus.webapp.PlusConfiguration
+import org.eclipse.jetty.server.Handler
+import org.eclipse.jetty.server.Server
+import org.eclipse.jetty.server.handler.HandlerList
+import org.eclipse.jetty.server.handler.ResourceHandler
+import org.eclipse.jetty.webapp.Configuration
+import org.eclipse.jetty.webapp.FragmentConfiguration
+import org.eclipse.jetty.webapp.MetaInfConfiguration
+import org.eclipse.jetty.webapp.WebAppContext
+import org.eclipse.jetty.webapp.WebInfConfiguration
+import org.eclipse.jetty.webapp.WebXmlConfiguration
+
+class WarlikeServer {
+    public static void main(String[] args) {
+        Server server = new Server(new Integer(args[0]))
+
+        WebAppContext context = new WebAppContext()
+        context.setResourceBase("build/main-web")
+        context.setConfigurations([
+            new AnnotationConfiguration(),
+            new WebXmlConfiguration(),
+            new WebInfConfiguration(),
+            new PlusConfiguration(),
+            new MetaInfConfiguration(),
+            new FragmentConfiguration(),
+            new EnvConfiguration()
+        ] as Configuration[])
+        context.setContextPath("/")
+        context.setParentLoaderPriority(true)
+
+        ResourceHandler resourceHandler = new ResourceHandler()
+        resourceHandler.setResourceBase("src/main/webapp")
+
+        HandlerList handlers = new HandlerList();
+        handlers.setHandlers([resourceHandler, context] as Handler[])
+        server.setHandler(handlers)
+        server.start()
+    }
+}
