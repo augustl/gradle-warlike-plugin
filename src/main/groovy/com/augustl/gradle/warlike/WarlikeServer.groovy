057ca2c4257eae3594b47de5076a168023c5d751
August Lilleaas <august@kodemaker.no>
2015-01-21 09:48:37 +0100

    Reverting - we actually need the resource handler first to avoid serving the resources from the webappcontext.

--- src/main/groovy/com/augustl/gradle/warlike/WarlikeServer.groovy
+++ src/main/groovy/com/augustl/gradle/warlike/WarlikeServer.groovy
@@ -36,7 +36,7 @@ class WarlikeServer {
         resourceHandler.setResourceBase("src/main/webapp")
 
         HandlerList handlers = new HandlerList();
-        handlers.setHandlers([context, resourceHandler] as Handler[])
+        handlers.setHandlers([resourceHandler, context] as Handler[])
         server.setHandler(handlers)
         server.start()
     }
