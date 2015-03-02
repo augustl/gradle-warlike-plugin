bc42094ec37be41e0d906bfa94e514cf91fdda21
August Lilleaas <august@kodemaker.no>
2015-03-02 13:12:01 +0100

    Correctly handling priority between servlet context mappings and static resources.
    Turns out that the initial implementation where the static resources
    took presedence over mapped context was wrong. The mapped contexts
    always gets presedence in tomcat, jetty, etc.
    
    This means that we can piggyback on Jetty's built in ResourceCollection,
    since all we really want is for Jetty to look for our resources in both
    the build directory and in the src/main/webapp directory that the war
    plugin uses.

--- src/main/groovy/com/augustl/gradle/warlike/WarlikeServer.groovy
+++ src/main/groovy/com/augustl/gradle/warlike/WarlikeServer.groovy
@@ -4,11 +4,9 @@ import org.eclipse.jetty.annotations.AnnotationConfiguration
 import org.eclipse.jetty.plus.webapp.EnvConfiguration
 import org.eclipse.jetty.plus.webapp.PlusConfiguration
 import org.eclipse.jetty.server.Handler
-import org.eclipse.jetty.server.Request
 import org.eclipse.jetty.server.Server
 import org.eclipse.jetty.server.handler.HandlerList
-import org.eclipse.jetty.server.handler.ResourceHandler
-import org.eclipse.jetty.util.resource.Resource
+import org.eclipse.jetty.util.resource.ResourceCollection
 import org.eclipse.jetty.webapp.Configuration
 import org.eclipse.jetty.webapp.FragmentConfiguration
 import org.eclipse.jetty.webapp.MetaInfConfiguration
@@ -16,16 +14,13 @@ import org.eclipse.jetty.webapp.WebAppContext
 import org.eclipse.jetty.webapp.WebInfConfiguration
 import org.eclipse.jetty.webapp.WebXmlConfiguration
 
-import javax.servlet.ServletException
-import javax.servlet.http.HttpServletRequest
-import javax.servlet.http.HttpServletResponse
-
 class WarlikeServer {
     public static void main(String[] args) {
         Server server = new Server(new Integer(args[0]))
 
         WebAppContext context = new WebAppContext()
-        context.setResourceBase("build/main-web")
+        ResourceCollection resources = new ResourceCollection(["build/main-web", "src/main/webapp"] as String[])
+        context.setBaseResource(resources)
         context.setConfigurations([
             new AnnotationConfiguration(),
             new WebXmlConfiguration(),
@@ -38,32 +33,9 @@ class WarlikeServer {
         context.setContextPath("/")
         context.setParentLoaderPriority(true)
 
-        ResourceHandler resourceHandler = new CustomResourceHandler()
-        resourceHandler.setResourceBase("src/main/webapp")
-
         HandlerList handlers = new HandlerList();
-        handlers.setHandlers([resourceHandler, context] as Handler[])
+        handlers.setHandlers([context] as Handler[])
         server.setHandler(handlers)
         server.start()
-    }
-
-    static final class CustomResourceHandler extends ResourceHandler {
-        @Override
-        void handle(String target, Request baseRequest, HttpServletRequest request, HttpServletResponse response) throws IOException, ServletException {
-            if (baseRequest.isHandled()) {
-                return
-            }
-
-            if (request.getRequestURI().equals("/")) {
-                Resource resource = getResource(request);
-                if (getWelcome(resource)) {
-                    super.handle(target, baseRequest, request, response)
-                } else {
-                    return
-                }
-            } else {
-                super.handle(target, baseRequest, request, response)
-            }
-        }
     }
 }
